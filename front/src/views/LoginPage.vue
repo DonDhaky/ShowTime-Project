27c4c01ae1d0a87c49c61fefee4c43bfcd65021b
DonDhaky <erick.thonon@epitech.eu>
2024-04-16 12:56:33 +0200

    [ADD] Base front

---
+++ front/src/views/LoginPage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+const submitLogin = () => {
+  //
+}
+</script>
+
+
+<template>
+
+    <div>
+      <form @submit="submitLogin">
+        <label for="email">Email :</label>
+        <input type="email" id="email" v-model="email" required>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" v-model="password" required>
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+    
+  </template>
+
+  
