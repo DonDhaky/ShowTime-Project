d3bd1f7960b2a294b7bff1b5c6bfdff724880832
DonDhaky <erick.thonon@epitech.eu>
2024-04-16 15:44:17 +0200

    [EDIT] App.vue + css

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,9 +9,14 @@ import { RouterLink, RouterView } from 'vue-router'
 
     <div class="wrapper">
       <nav>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
+        <RouterLink to="/home">Accueil</RouterLink>
+        <RouterLink to="/profil">Mon Compte</RouterLink>
+        <RouterLink to="/login">Connexion</RouterLink>
+        <RouterLink to="/register">Inscription</RouterLink>
+        <RouterLink to="/whishlist">Favoris</RouterLink>
+        <RouterLink to="/panier">Panier</RouterLink>
       </nav>
+
       <RouterView />
     </div>
 
@@ -19,22 +24,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
 nav a.router-link-exact-active {
   color: var(--color-text);
@@ -54,30 +43,8 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
+nav {
+    text-align: center;
     padding: 1rem 0;
-    margin-top: 1rem;
   }
-}
 </style>
