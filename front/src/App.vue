bb4363f2109560d0c50843d205f30457ec786300
DonDhaky <erick.thonon@epitech.eu>
2024-04-17 12:19:17 +0200

    [ADD+EDIT] Navbar + homepage

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,41 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
+import ShowsList from "../src/components/ShowsList.vue";
 
+function logout() {
+  //
+  alert("Vous êtes bien déconnecté(e) !");
+}
 </script>
 
-
 <template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/home">Accueil</RouterLink>
+      <RouterLink to="/profile">Mon Compte</RouterLink>
+      <RouterLink to="/login">Connexion</RouterLink>
+      <RouterLink to="/home" @click="logout">Déconnexion</RouterLink>
+      <RouterLink to="/register">Inscription</RouterLink>
+      <RouterLink to="/wishlist">Favoris</RouterLink>
+      <RouterLink to="/card">Panier</RouterLink>
+      <RouterLink to="/admindashboard">Admin</RouterLink>
+    </nav>
 
+    <RouterView />
+  </div>
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/home">Accueil</RouterLink>
-        <RouterLink to="/profil">Mon Compte</RouterLink>
-        <RouterLink to="/login">Connexion</RouterLink>
-        <RouterLink to="/register">Inscription</RouterLink>
-        <RouterLink to="/whishlist">Favoris</RouterLink>
-        <RouterLink to="/panier">Panier</RouterLink>
-      </nav>
-
-      <RouterView />
-    </div>
-
-
+  <div class="showslist">
+    <ShowsList />
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
 
 nav a.router-link-exact-active {
   color: var(--color-text);
@@ -44,7 +56,14 @@ nav a:first-of-type {
 }
 
 nav {
-    text-align: center;
-    padding: 1rem 0;
-  }
+  text-align: center;
+  padding: 1rem 0;
+}
+
+.showslist {
+  text-align: left;
+  margin-left: 30%;
+  margin-top: 10%;
+  margin-right: 2rem;
+}
 </style>
--- front/src/views/RegisterPage.vue
+++ front/src/views/RegisterPage.vue
@@ -1,28 +1,44 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const email = ref('')
-const password = ref('')
+const email = ref("");
+const password = ref("");
 
 const submitRegister = () => {
   //
-}
+  alert("Vous êtes bien inscrit(e) sur Los Ticketos !");
+};
 </script>
 
 <template>
+  <div>
+    <form @submit="submitRegister" class="form">
+      <h1 class="title">Inscrivez-vous sur Los Ticketos</h1>
+      <label for="email">Email :</label>
+      <input type="email" id="email" v-model="email" required />
+      <label for="password">Mot de passe :</label>
+      <input type="password" id="password" v-model="password" required />
+      <button type="submit">S'inscrire</button>
+    </form>
+  </div>
+</template>
 
-    <div>
-      <form @submit="submitRegister">
-        <label for="email">Email :</label>
-        <input type="email" id="email" v-model="email" required>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" v-model="password" required>
-        <button type="submit">S'inscrire</button>
-      </form>
-    </div>
-    
-  </template>
+<style scoped>
+.form {
+  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  max-width: 500px;
+  margin: 0 auto;
+  margin-top: 10%;
+}
 
-  
+.form label,
+.form button {
+  margin-top: 30px;
+}
 
-  
+.title {
+  text-align: center;
+}
+</style>
