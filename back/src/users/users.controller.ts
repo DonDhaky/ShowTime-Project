8bf3777c873bcdabb0789ff19b92fc4e3c3ec99b
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-04-18 15:38:40 +0200

    small structure cleaning

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -8,44 +8,44 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
-    //POST route
-    @Post('/signup')
-    async createUser(@Body() createUserDto: CreateUserDto) {
-      const result = await this.UserService.createUser(createUserDto);
-      return {
-        msg: 'Concert successfully registered',
-        result,
-        end: 'message end'
-      };
-    }
-
-    //GET routes
-    @Get()
-    async getConcerts(): Promise<User[]> {
-      return await this.UserService.getUsers();
-    }
-    @Get(':id')
-    async getUserById(@Param('id') id: string): Promise<User> {
-      return await this.UserService.getUserById(id);
-    }
-    @Get('/byemail/:email')
-    async getUserByEmail(@Param('email') email: string): Promise<User> {
-      return await this.UserService.getUserByEmail(email);
-    }
-
-    //UPDATE routes
-    @Put(':id')
-    async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-      return await this.UserService.updateUser(id, updateUserDto);
-    }
-    @Patch(':id')
-    async patchUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-      return await this.UserService.patchUser(id, updateUserDto);
-    }
-
-    //DELETE route
-    @Delete(':id')
-    async deleteUser(@Param('id') id: string): Promise<User> {
-      return await this.UserService.deleteUser(id);
-    }
+  //POST route
+  @Post('/signup')
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const result = await this.UserService.createUser(createUserDto);
+    return {
+      msg: 'Concert successfully registered',
+      result,
+      end: 'message end'
+    };
   }
+
+  //GET routes
+  @Get()
+  async getConcerts(): Promise<User[]> {
+    return await this.UserService.getUsers();
+  }
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    return await this.UserService.getUserById(id);
+  }
+  @Get('/byemail/:email')
+  async getUserByEmail(@Param('email') email: string): Promise<User> {
+    return await this.UserService.getUserByEmail(email);
+  }
+
+  //UPDATE routes
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.UserService.updateUser(id, updateUserDto);
+  }
+  @Patch(':id')
+  async patchUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.UserService.patchUser(id, updateUserDto);
+  }
+
+  //DELETE route
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return await this.UserService.deleteUser(id);
+  }
+}
