ac4aea8aa9cf8bcf52a86d59a73b737a6002df17
DonDhaky <erick.thonon@epitech.eu>
2024-04-19 10:30:07 +0200

    [EDIT] pages de la navbar

--- front/src/views/AdminPage.vue
+++ front/src/views/AdminPage.vue
@@ -12,15 +12,14 @@ const number_of_bookings = ref('')
 // USERS VARIABLES
 const users = ref([])
 const email = ref('')
-const username = ref('')
 const is_admin = ref('')
 
 
 // AFFICHER LES USERS //////////////////////////////////////////////////////////////////////////// 
 const fetchUsers = async () => {
   const response = await fetch('http://localhost:3000/users');
-  const data = await response.json();
-  users.value = data;
+  const dataUsers = await response.json();
+  users.value = dataUsers;
   console.log('Liste des users :', users.value);
 };
 
@@ -38,8 +37,8 @@ const submitDeleteUser = () => {
 // AFFICHER LES CONCERTS //////////////////////////////////////////////////////////////////////////////
 const fetchConcerts = async () => {
   const response1 = await fetch('http://localhost:3000/concerts');
-  const data = await response1.json();
-  concerts.value = data;
+  const dataConcerts = await response1.json();
+  concerts.value = dataConcerts;
   console.log('Liste des concerts :', concerts.value);
 };
 
@@ -69,13 +68,13 @@ const submitDeleteShow = () => {
     <h1>Liste des concerts :</h1>
     <div class="concert" v-for="concert in concerts" :key="concert.id">
       <p>Nom du groupe :</p>
-      <input v-model="concert.group">
+      <input v-model="concert.group" required />
       <p>Date du concert :</p>
-      <input v-model="concert.date">
+      <input v-model="concert.date" required />
       <p>Genre :</p>
-      <input v-model="concert.genre">
+      <input v-model="concert.genre" required />
       <p>Prix :</p>
-      <input v-model="concert.price">
+      <input v-model="concert.price" required />
       <button @click="submitEditShow">Modifier</button>
       <button @click="submitDeleteShow">Supprimer</button>
     </div>
@@ -84,12 +83,10 @@ const submitDeleteShow = () => {
   <div class="users-container">
     <h1>Liste des users :</h1>
     <div class="user" v-for="user in users" :key="user.id">
-      <p>Username :</p>
-      <input v-model="user.username">
       <p>Email :</p>
-      <input v-model="user.email">
+      <input v-model="user.email" required />
       <p>Admin ?</p>
-      <input v-model="user.is_admin">
+      <input v-model="user.is_admin" required />
       <button @click="submitEditUser">Modifier</button>
       <button @click="submitDeleteUser">Supprimer</button>
     </div>
